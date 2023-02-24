bf031b643742bad685fa82dfa3b89fdb288e462a
unknown <narmada.vaddireddy97@gmail.com>
2023-02-24 12:53:24 +0530

    Added: Code for above 100 position player will be in same poisition

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -5,7 +5,7 @@ public class SnakeAndLadder {
 
     public static void main(String[] args) {
         int position = 0;
-        while (position <= 100) {
+        while (position != 100) {
             int roll = (int) Math.floor((Math.random() * 10) % 3);
             int dice = (int) Math.floor((Math.random() * 10) % 6) + 1;
             switch (roll) {
@@ -18,14 +18,19 @@ public class SnakeAndLadder {
                         position = position - dice;
                     }
                 case LADDER:
-                    System.out.println("player goes to next position : " + position);
-                    position = position + dice;
+                    if (position > 100) {
+                        System.out.println("player got extra number : " + position);
+                        position = position - dice;
+                    } else {
+                        System.out.println("player is in next position : " + position);
+                        position = position + dice;
+                    }
                     break;
                 case NO_PLAY:
                     System.out.println("no Play");
                     break;
             }
         }
-        System.out.println("player winning position : " + position);
+        System.out.println("winning position : " + position);
     }
 }
