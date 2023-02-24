63aab9f56575b13dc4c3d45eb2b221d66aed286b
unknown <narmada.vaddireddy97@gmail.com>
2023-02-24 12:15:56 +0530

    Added: Code to roll the die between 1 to 6

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,6 +1,6 @@
 public class SnakeAndLadder {
     public static void main(String[] args) {
-        int position = 0;
-        System.out.println("Player starting position : " + position);
+        int dice = (int) (Math.random() * 10) % 6 + 1;
+        System.out.println(" dice roll to get The number Between1 to 6 : " + dice);
     }
 }
