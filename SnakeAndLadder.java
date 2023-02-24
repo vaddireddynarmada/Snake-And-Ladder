4c566f995a977bdd74a4e6dac01f678a28412d75
unknown <narmada.vaddireddy97@gmail.com>
2023-02-24 12:21:36 +0530

    Added: Code to check the position of player

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,6 +1,24 @@
 public class SnakeAndLadder {
+    static final int NO_PLAY = 0;
+    static final int LADDER = 1;
+    static final int SNAKE = 2;
     public static void main(String[] args) {
-        int dice = (int) (Math.random() * 10) % 6 + 1;
-        System.out.println(" dice roll to get The number Between1 to 6 : " + dice);
+        int position = 0;
+        while (position <= 100) {
+            int roll = (int) Math.floor((Math.random() * 10) % 3);
+            int dice = (int) Math.floor((Math.random() * 10) % 6) + 1;
+            switch (roll) {
+                case SNAKE:
+                    System.out.println("player got snake : " + position);
+                    position = position - dice;
+                case LADDER:
+                    System.out.println("player goes to next position : " + position);
+                    position = position + dice;
+                    break;
+                case NO_PLAY:
+                    System.out.println("no Play");
+                    break;
+            }
+        }
     }
 }
