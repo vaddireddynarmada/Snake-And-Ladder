44df5bf628135c752c5cb7df83a43094269a2bea
unknown <narmada.vaddireddy97@gmail.com>
2023-02-24 12:06:49 +0530

    Added: Code for single player starting position

---
+++ SnakeAndLadder.java
@@ -0,0 +1,6 @@
+public class SnakeAndLadder {
+    public static void main(String[] args) {
+        int position = 0;
+        System.out.println("Player starting position : " + position);
+    }
+}
