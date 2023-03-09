ff65ff250b3de49a8bcd3e71b7cbd119ac70cede
unknown <narmada.vaddireddy97@gmail.com>
2023-03-09 09:19:49 +0530

    Added: Code for two players who is winner

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,38 +1,64 @@
-public class SnakeAndLadder {
-    static final int NO_PLAY = 0;
-    static final int LADDER = 1;
-    static final int SNAKE = 2;
+import java.util.Random;
 
-    public static void main(String[] args) {
-        int position = 0,rollCount=0;
-        while (position != 100) {
-            rollCount++;
-            int roll = (int) Math.floor((Math.random() * 10) % 3);
-            int dice = (int) Math.floor((Math.random() * 10) % 6) + 1;
-            switch (roll) {
-                case SNAKE:
-                    if (position < 0) {
-                        position = 0;
-                        System.out.println("start from zero: " + position);
-                    } else {
-                        System.out.println("player got snake : " + position);
-                        position = position - dice;
-                    }
-                case LADDER:
-                    if (position > 100) {
-                        System.out.println("player got extra number : " + position);
-                        position = position - dice;
-                    } else {
-                        System.out.println("player is in next position : " + position);
-                        position = position + dice;
-                    }
-                    break;
-                case NO_PLAY:
-                    System.out.println("no Play");
-                    break;
+public class SnakeAndLadder {
+    public static void playerWin() {
+        int winningPosition = 100;
+        int playerPosition1 = 0;
+        int playerPosition2 = 0;
+        int count = 0;
+        Random random = new Random();
+        while ((playerPosition1 != winningPosition) && (playerPosition2 != winningPosition)) {
+            count++;
+            int rollTheDice1 = random.nextInt(6) + 1;
+            int rollTheDice2 = random.nextInt(6) + 1;
+            System.out.println("Player1 after rolling the dice : " + rollTheDice1);
+            System.out.println("Player2 after rolling the dice : " + rollTheDice2);
+            int optionCheck1 = random.nextInt(3);
+            int optionCheck2 = random.nextInt(3);
+            if (optionCheck1 == 0) {
+                System.out.println("Player1 No play : " + playerPosition1);
+            } else if (optionCheck1 == 1) {
+                if (playerPosition1 + rollTheDice1 > winningPosition) {
+                    playerPosition1 -= rollTheDice1;
+                    System.out.println("Player1 Ladder : " + playerPosition1);
+                } else {
+                    playerPosition1 += rollTheDice1;
+                    System.out.println("Player1 Ladder : " + playerPosition1);
+                }
+            } else {
+                playerPosition1 -= rollTheDice1;
+                if (playerPosition1 < 0)
+                    playerPosition1 = 0;
+                System.out.println("Player1 Snake : " + playerPosition1);
+            }
+            if (optionCheck2 == 0) {
+                System.out.println("Player2 No play : " + playerPosition2);
+            } else if (optionCheck2 == 1) {
+                if (playerPosition2 + rollTheDice2 > winningPosition) {
+                    playerPosition2 -= rollTheDice2;
+                    System.out.println("Player2 Ladder : " + playerPosition2);
+                } else {
+                    playerPosition2 += rollTheDice2;
+                    System.out.println("Player2 Ladder : " + playerPosition2);
+                }
+            } else {
+                playerPosition2 -= rollTheDice2;
+                if (playerPosition2 < 0)
+                    playerPosition2 = 0;
+                System.out.println("Player2 Snake : " + playerPosition2);
             }
         }
-        System.out.println("winning position : " + position);
-        System.out.println("No of times dice rolled = " + rollCount);
+        System.out.println("Final position of player1 : " + playerPosition1);
+        System.out.println("Final position of player2 : " + playerPosition2);
+        System.out.println("Number Of Total Moves : " + count);
+        if (playerPosition1 > playerPosition2) {
+            System.out.println("Player1 Win The Game");
+        } else {
+            System.out.println("Player2 Win The Game");
+        }
+    }
+
+    public static void main(String[] args) {
+        playerWin();
     }
 }
